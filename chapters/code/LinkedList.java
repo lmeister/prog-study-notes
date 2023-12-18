2c097b535495d5e3572afdb65f31e97c41c0b6e1
Leon Meister <Leonmeister97@gmail.com>
2023-12-18 13:02:46 +0100

    Add LinkedList

---
+++ chapters/code/LinkedList.java
@@ -0,0 +1,16 @@
+// Import der LinkedList-Klasse
+import java.util.LinkedList;
+
+// Deklaration und Initialisierung (singly LinkedList)
+LinkedList<Integer> myLinkedList = new LinkedList<>();
+
+// Elemente hinzufügen
+myLinkedList.add(1);
+
+// Zugriff auf ein Element
+int element = myLinkedList.get(1);
+
+// Iteration durch die LinkedList
+for (int value : myLinkedList) {
+    System.out.println(value);
+}
